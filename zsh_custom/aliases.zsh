21dd5f6f512c2b10f16c6c5300cb893fd2904eb5
Hendrik Bursian <hendrikbursian@protonmail.com>
2019-05-10 12:20:10 +0200

    Added alias for git commit --fixup

--- zsh_custom/aliases.zsh
+++ zsh_custom/aliases.zsh
@@ -5,6 +5,7 @@ alias plug='load_plugins'
 alias grb='git rebase -i origin/develop'
 alias gdev='git checkout develop && git pull'
 alias gpf='git push --force-with-lease'
+alias gcf='git commit --fixup'
 
 # wobcom
 alias bmock='/home/hendrik/workspace/cssp/abestmockadapter/contrib/build.ps1'
