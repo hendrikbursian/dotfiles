2dad614667bb1ba6f286b7ce35535edd4c6367f4
Hendrik Bursian <hendrikbursian@protonmail.com>
2019-05-11 16:14:51 +0200

    Add documentation

--- zsh_custom/plugins.zsh
+++ zsh_custom/plugins.zsh
@@ -1,1 +1,6 @@
-
+# Plugins for zsh to load
+# After modifying this file call `plug`.
+# This will statically load the plugins with antibody.
+#
+# Entries can be added like this:
+# natterstefan/toggl-zsh-plugin
