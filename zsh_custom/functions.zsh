765b512a81cfa80d7304e9729ba606b3087cae5d
Hendrik Bursian <hendrikbursian@protonmail.com>
2019-05-11 16:47:15 +0200

    Add timer function for zsh

--- zsh_custom/functions.zsh
+++ zsh_custom/functions.zsh
@@ -14,3 +14,10 @@ function load_plugins() {
     echo ""
     echo "Done"
 }
+
+function timer() {
+    if [ $1 -gt 5]; then
+        at now + $1 minutes <<<'notify-send 5 minutes left for '$2
+    fi
+    at now + $1 minutes <<<'notify-send timer expired for '$2
+}
