29c9081917f01db0a8ff14b08260c41b09bae0eb
Hendrik Bursian <hendrikbursian@protonmail.com>
2019-05-11 22:48:25 +0200

    remove timer function

--- zsh_custom/functions.zsh
+++ zsh_custom/functions.zsh
@@ -14,10 +14,3 @@ function load_plugins() {
     echo ""
     echo "Done"
 }
-
-function timer() {
-    if [ $1 -gt 5]; then
-        at now + $1 minutes <<<'notify-send 5 minutes left for '$2
-    fi
-    at now + $1 minutes <<<'notify-send timer expired for '$2
-}
