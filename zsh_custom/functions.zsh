5f28ec589dc1b77cbfcf87c400b03a0f8103ddd5
Hendrik Bursian <hendrikbursian@protonmail.com>
2019-05-12 18:30:52 +0200

    Set default transparency to 90

--- zsh_custom/functions.zsh
+++ zsh_custom/functions.zsh
@@ -18,7 +18,7 @@ function __load_plugins() {
 function __set_transparency() {
     local transparency=$1
     
-    [[ ! -z "$transparency" ]] || transparency=95
+    [[ ! -z "$transparency" ]] || transparency=90
 
     sh -c "xprop -f _NET_WM_WINDOW_OPACITY 32c -set _NET_WM_WINDOW_OPACITY $(printf 0x%x $((0xffffffff * transparency / 100)))"
 }
