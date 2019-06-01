f4ecb5aed3d3a6a2aef0850835a293be4df1806b
Hendrik Bursian <hendrikbursian@protonmail.com>
2019-06-01 21:36:24 +0200

    Update

--- remove-links.sh
+++ remove-links.sh
@@ -4,5 +4,5 @@ DIR=$(dirname "$(readlink -f "$0")")
 filelist="$DIR/filelist"
 while IFS=" " read -r homefile target
 do
-    [[ -n "$homefile" ]] && [[ -n "$target" ]] && rm -vr "$HOME:?/$homefile"
+    [[ -n "$homefile" ]] && [[ -n "$target" ]] && rm -vr "${HOME:?}/$homefile"
 done < "$filelist"
