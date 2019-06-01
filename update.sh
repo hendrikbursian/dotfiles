9aea3d0b4208857ef6933e24c581f7e7be9476d1
Hendrik Bursian <hendrikbursian@protonmail.com>
2019-06-01 21:31:20 +0200

    Fix Shellcheck annotations

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
-DIR=$(dirname $(readlink -f "$0"))
-cd "$DIR"
+DIR=$(dirname "$(readlink -f "$0")")
+cd "$DIR" || exit 1
 
 ./remove-links.sh
 
@@ -8,4 +8,4 @@ git pull --recurse-submodules
 
 ./add-links.sh
 
-cd "$OLDDIR"
+cd "$OLDDIR" || exit 1
