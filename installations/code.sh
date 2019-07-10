cf031da1dae973d0d88034613c2b95c7586ad950
Hendrik Bursian <hendrikbursian@protonmail.com>
2019-07-10 09:19:08 +0200

    code insiders extensions

--- installations/code.sh
+++ installations/code.sh
@@ -1,19 +1,25 @@
 #! /usr/bin/env bash
 
 # vscode
-sudo snap install code --classic
-code --install-extension anjali.clipboard-history
-code --install-extension dotjoshjohnson.xml
-code --install-extension eamodio.gitlens
-code --install-extension eg2.tslint
-code --install-extension esbenp.prettier-vscode
-code --install-extension foxundermoon.shell-format
-code --install-extension peterjausovec.vscode-docker
-code --install-extension pflannery.vscode-versionlens
-code --install-extension ryu1kn.partial-diff
-code --install-extension vscodevim.vim
-code --install-extension timonwong.shellcheck
-code --install-extension wakatime.vscode-wakatime
-code --install-extension fatihacet.gitlab-workflow
-code --install-extension ms-python.python
-code --install-extension exiasr.hadolint
+sudo snap install code-insiders --classic
+code-insiders --install-extension anjali.clipboard-history
+code-insiders --install-extension dotjoshjohnson.xml
+code-insiders --install-extension eamodio.gitlens
+code-insiders --install-extension eg2.tslint
+code-insiders --install-extension esbenp.prettier-vscode
+code-insiders --install-extension foxundermoon.shell-format
+code-insiders --install-extension peterjausovec.vscode-docker
+code-insiders --install-extension pflannery.vscode-versionlens
+code-insiders --install-extension ryu1kn.partial-diff
+code-insiders --install-extension vscodevim.vim
+code-insiders --install-extension timonwong.shellcheck
+code-insiders --install-extension wakatime.vscode-wakatime
+code-insiders --install-extension fatihacet.gitlab-workflow
+code-insiders --install-extension ms-python.python
+code-insiders --install-extension exiasr.hadolint
+code-insiders --install-extension ms-vsliveshare.vsliveshare
+code-insiders --install-extension ms-vscode-remote.remote-ssh
+code-insiders --install-extension ms-vscode-remote.remote-container
+code-insiders --install-extension ms-vscode-remote.vscode-remote-extensionpack
+
+
