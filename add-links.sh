b7ed9a5fc5ce280e07740a3134c659ad290f1f36
Hendrik <hendrikbursian@protonmail.com>
2019-07-10 03:21:42 +0200

    Update

--- add-links.sh
+++ add-links.sh
@@ -3,7 +3,7 @@ DIR=$(dirname "$(readlink -f "$0")")
 
 echo "Link dotfiles to home directory"
 filelist="$DIR/filelist"
-while IFS=" -> " read -r homefile target; do
+while IFS="@" read -r homefile target; do
     # Prevent creating duplicated links for directories
     if [[ -L "$HOME/$homefile" && "$(readlink "$HOME/$homefile")" == "$DIR/$target" ]]; then
         printf "'%s' -> '%s'\n" "$HOME/$homefile" "$DIR/$target"
