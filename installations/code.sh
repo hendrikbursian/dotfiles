e5849058c405cb71f0b68ce491407a0a483fea32
Hendrik Bursian <hendrikbursian@protonmail.com>
2019-09-15 20:20:19 +0200

    Update

--- installations/code.sh
+++ installations/code.sh
@@ -2,23 +2,38 @@
 
 # vscode-insiders
 sudo snap install code-insiders --classic
-code-insiders --install-extension exiasr.hadolint
-code-insiders --install-extension timonwong.shellcheck
-code-insiders --install-extension esbenp.prettier-vscode
-code-insiders --install-extension foxundermoon.shell-format
-code-insiders --install-extension wakatime.vscode-wakatime
-code-insiders --install-extension anjali.clipboard-history
-code-insiders --install-extension fatihacet.gitlab-workflow
+
+code-insiders --install-extension angular.ng-template
+code-insiders --install-extension arcticicestudio.nord-visual-studio-code
+code-insiders --install-extension auchenberg.vscode-browser-preview
+code-insiders --install-extension coolbear.systemd-unit-file
+code-insiders --install-extension cyrilletuzi.angular-schematics
+code-insiders --install-extension dbaeumer.vscode-eslint
 code-insiders --install-extension dotjoshjohnson.xml
-code-insiders --install-extension pflannery.vscode-versionlens
-code-insiders --install-extension peterjausovec.vscode-docker
+code-insiders --install-extension drewbourne.vscode-remark-lint
 code-insiders --install-extension eamodio.gitlens
-code-insiders --install-extension mtxr.sqltools
+code-insiders --install-extension editorconfig.editorconfig
+code-insiders --install-extension esbenp.prettier-vscode
+code-insiders --install-extension exiasr.hadolint
+code-insiders --install-extension fatihacet.gitlab-workflow
+code-insiders --install-extension foxundermoon.shell-format
+code-insiders --install-extension gencer.html-slim-scss-css-class-completion
+code-insiders --install-extension hangxingliu.vscode-systemd-support
 code-insiders --install-extension humao.rest-client
-code-insiders --install-extension ms-vscode.vscode-typescript-tslint-plugin
+code-insiders --install-extension mikael.angular-beastcode
+code-insiders --install-extension ms-azuretools.vscode-docker
+code-insiders --install-extension ms-mssql.mssql
 code-insiders --install-extension ms-python.python
-code-insiders --install-extension ms-vsliveshare.vsliveshare
+code-insiders --install-extension ms-vscode-remote.remote-containers
 code-insiders --install-extension ms-vscode-remote.remote-ssh
-code-insiders --install-extension ms-vscode-remote.remote-container
+code-insiders --install-extension ms-vscode-remote.remote-ssh-edit
+code-insiders --install-extension ms-vscode-remote.remote-ssh-explorer
+code-insiders --install-extension ms-vscode-remote.remote-wsl
 code-insiders --install-extension ms-vscode-remote.vscode-remote-extensionpack
-code-insiders --install-extensions arcticicestudio.nord-visual-studio-code
+code-insiders --install-extension ms-vscode.vscode-typescript-tslint-plugin
+code-insiders --install-extension ms-vsliveshare.vsliveshare
+code-insiders --install-extension mtxr.sqltools
+code-insiders --install-extension pflannery.vscode-versionlens
+code-insiders --install-extension shanoor.vscode-nginx
+code-insiders --install-extension timonwong.shellcheck
+code-insiders --install-extension wakatime.vscode-wakatime
