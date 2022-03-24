781a2ef67f4cbfda3b97a36410b1289246d27066
Hendrik Bursian <hendrikbursian@pm.me>
2022-03-24 23:25:46 +0100

    Update telescope config

--- nvim/.config/nvim/lua/hendrik/telescope.lua
+++ nvim/.config/nvim/lua/hendrik/telescope.lua
@@ -7,38 +7,40 @@ local previewers = require("telescope.previewers")
 local conf = require("telescope.config").values
 
 require("telescope").setup{
---    defaults = {
---        file_sorter = require("telescope.sorters").get_fzy_sorter,
---        prompt_prefix = " >",
---        color_devicons = true,
---        shorten_path = true,
---
---        file_previewer = require("telescope.previewers").vim_buffer_cat.new,
---        grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
---        qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
---
---        mappings = {
---            i = {
---                ["<C-x>"] = false,
---                ["<C-q>"] = actions.send_to_qflist,
---            },
---        },
---    },
---
---    extensions = {
---        fzy_native = {
---            override_generic_sorter = false,
---            override_file_sorter = true,
---        },
---    },
+    defaults = {
+        file_sorter = require("telescope.sorters").get_fzy_sorter,
+        prompt_prefix = " >",
+        color_devicons = true,
+
+        file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+        grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+        qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+
+        mappings = {
+            i = {
+                ["<C-x>"] = false,
+                ["<C-q>"] = actions.send_to_qflist,
+            },
+        },
+    },
+
+    extensions = {
+        fzy_native = {
+            override_generic_sorter = false,
+            override_file_sorter = true,
+        },
+    },
 }
--- require("telescope").load_extension("fzy_native")
+require("telescope").load_extension("fzy_native")
 
 local M = {}
 
 -- 'git_files' if repo 'find_files' otherwise
 function M.project_files()
-    local opts = {} -- define here if you want to define something
+    local opts = {
+        hidden = true,
+        shorten_paths = true,
+    }
     local ok = pcall(require("telescope.builtin").git_files, opts)
     if not ok then require("telescope.builtin").find_files(opts) end
 end
