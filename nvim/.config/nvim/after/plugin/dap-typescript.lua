1c0f038c0302e98b547860115a73cf889cb7e1cd
Hendrik Bursian <hendrikbursian@pm.me>
2022-11-11 20:54:17 +0100

    Debugger typescript

--- nvim/.config/nvim/after/plugin/dap-typescript.lua
+++ nvim/.config/nvim/after/plugin/dap-typescript.lua
@@ -5,23 +5,28 @@ require("dap-vscode-js").setup({
     adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
 })
 
-for _, language in ipairs({ "typescript", "javascript" }) do
-    require("dap").configurations[language] = {
-        {
-            type = "pwa-node",
-            request = "launch",
-            name = "Launch file",
-            program = "${file}",
-            cwd = "${workspaceFolder}",
-        },
-        {
-            type = "pwa-node",
-            request = "attach",
-            name = "Attach",
-            processId = require 'dap.utils'.pick_process,
-            cwd = "${workspaceFolder}",
+require('dap.ext.vscode').load_launchjs(nil, { ['pwa-node'] = { 'typescript' } })
+
+if require('dap').configurations.typescript == nil then
+    for _, language in ipairs({ "typescript", "javascript" }) do
+        require("dap").configurations[language] = {
+            {
+                type = "pwa-node",
+                request = "launch",
+                name = "Launch file",
+                program = "${file}",
+                cwd = "${workspaceFolder}",
+            },
+            {
+                type = "pwa-node",
+                request = "attach",
+                name = "Attach",
+                processId = require 'dap.utils'.pick_process,
+                cwd = "${workspaceFolder}",
+            }
         }
-    }
+    end
 end
+
 
 require('dap').set_log_level('TRACE')
