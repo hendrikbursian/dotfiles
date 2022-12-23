c10bfe10cfb730d83b22f7fb296fb7920c03d754
Hendrik Bursian <hendrikbursian@pm.me>
2022-12-23 12:30:06 +0100

    Update nvim-tree

--- nvim/.config/nvim/lua/hendrik/nvim-tree.lua
+++ nvim/.config/nvim/lua/hendrik/nvim-tree.lua
@@ -1,7 +1,5 @@
-
 local view = require "nvim-tree.view"
-local find_file = require "nvim-tree".find_file
-local open = require "nvim-tree".open
+local api = require "nvim-tree.api"
 
 local M = {}
 
@@ -9,13 +7,9 @@ M.toggle_focused_file = function()
     if view.is_visible() then
         view.close()
     else
-        local previous_buf = vim.api.nvim_get_current_buf()
-        find_file(false, previous_buf)
-
         local file_path = vim.fn.expand("%:h")
-        open(file_path)
+        api.tree.find_file(file_path)
     end
 end
 
 return M
-
