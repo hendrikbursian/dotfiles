91cbb8eb004739a6f06f8c5580e1f0993b44d523
Hendrik Bursian <hendrik.bursian@deutschebahn.com>
2023-09-23 11:19:29 +0200

    Improve system viewer under wsl

--- nvim/.config/nvim/lua/hendrik/init.lua
+++ nvim/.config/nvim/lua/hendrik/init.lua
@@ -2,3 +2,5 @@ require("hendrik.sets")
 require("hendrik.plugins")
 require("hendrik.colors")
 require("hendrik.filetype")
+
+require('nvim-tree.actions.node.system-open').config.system_open.cmd = 'wslview'
