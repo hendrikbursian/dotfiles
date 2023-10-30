80b787e2e7a483ba7fafc019afb57c387576d772
Hendrik Bursian <hendrik.bursian@deutschebahn.com>
2023-10-30 09:03:37 +0100

    Mess around with ansible

--- ansible/ansible.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-ansible-galaxy install -r requirements.yml
-ansible-playbook -v -i $HOME/.dotfiles/ansible/hosts $HOME/.dotfiles/ansible/main.yml --ask-become-pass --ask-vault-pass
---
+++ ansible/scripts/install_ansible.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/sh
+
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible --yes
