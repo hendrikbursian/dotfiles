5274ca753c931e1b753afea537bf993053cb1900
Hendrik Bursian <hendrikbursian@pm.me>
2023-12-12 14:20:24 +0100

    Update ansible

--- ansible/scripts/install_ansible.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/sh
-
-sudo apt update
-sudo apt install software-properties-common
-sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible --yes
--- ansible/scripts/run_ansible.sh
+++ ansible/scripts/run_ansible.sh
@@ -6,4 +6,4 @@ cd "$(dirname "$0")" || exit
 ANSIBLE_HOME=..
 
 ansible-galaxy install -r "$ANSIBLE_HOME/requirements.yml"
-ansible-playbook -v -i "$ANSIBLE_HOME/hosts" "$ANSIBLE_HOME/main.yml"
+ansible-playbook -v -i "$ANSIBLE_HOME/hosts" "$ANSIBLE_HOME/000_main.yaml" --ask-become-pass --ask-vault-pass
