1ad67304f20665b5f7cfa73596d8d3806d2f5993
Hendrik Bursian <hendrikbursian@protonmail.com>
2024-08-08 15:13:35 +0200

    Add German locale

--- configuration.nix
+++ configuration.nix
@@ -38,7 +38,10 @@
   time.timeZone = "Europe/Berlin";
 
   i18n.defaultLocale = "de_DE.UTF-8";
-  i18n.supportedLocales = [ "en_US.UTF-8" ];
+  i18n.supportedLocales = [
+    "de_DE.UTF-8/UTF-8"
+    "en_US.UTF-8/UTF-8"
+  ];
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "de_DE.UTF-8";
     LC_IDENTIFICATION = "de_DE.UTF-8";
